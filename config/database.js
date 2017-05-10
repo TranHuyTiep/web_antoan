4bbd2e6e828633bee1324a6a143c85bb75199704
tranhuytiep <tranhuytiep95@gmail.com>
2017-05-10 08:51:17 +0700

    databae cleand

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@
  */
 var mysql =  require('mysql');
 var connection =  mysql.createConnection({
-    user:'sql10172121',
-    host:'sql10.freemysqlhosting.net',
-    password:'9uihiZrCms',
-    database:'sql10172121',
+    user:'b5ab7f6bd40958',
+    host:'us-cdbr-iron-east-03.cleardb.net',
+    password:'f0df253f',
+    database:'heroku_672101368c931b9',
 })
 connection.connect();
 module.exports = connection;
