8e24a5a9d86ddecfc852fbbe0acd7d43397dd0b8
tranhuytiep <tranhuytiep95@gmail.com>
2017-05-10 18:31:33 +0700

    databae cleand

--- config/database.js
+++ config/database.js
@@ -2,13 +2,14 @@
  * Created by Huy Tiep on 4/2/2017.
  */
 var mysql =  require('mysql');
-var connection =  mysql.createConnection({
+var connection =  mysql.createPool({
+    connectionLimit : 10,
     user:'b5ab7f6bd40958',
     host:'us-cdbr-iron-east-03.cleardb.net',
     password:'f0df253f',
     database:'heroku_672101368c931b9',
 })
-connection.connect();
+// connection.connect();
 //
 // function handleDisconnect() {
 //     connection = mysql.createConnection(db_config); // Recreate the connection, since
